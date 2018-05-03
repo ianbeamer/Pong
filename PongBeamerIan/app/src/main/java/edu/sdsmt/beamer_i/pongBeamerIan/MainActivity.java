cde0ad5a78fb318a39000419f90a09e47b60feb5
7283000 <ian.beamer@mines.sdsmt.edu>
2018-05-02 19:31:32 -0600

    try to make a pause and restart work

--- PongBeamerIan/app/src/main/java/edu/sdsmt/beamer_i/pongBeamerIan/MainActivity.java
+++ PongBeamerIan/app/src/main/java/edu/sdsmt/beamer_i/pongBeamerIan/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import edu.sdsmt.beamer_i.pongtest.R;
 
 public class MainActivity extends AppCompatActivity {
+    boolean Paused = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,21 +26,33 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    public void colorPicker(){
+    public void colorPicker(View view){
         onPause();
         //open that window
         onResume();
     }
 
-    public void tutorial(){
+    public void tutorial(View view){
         onPause();
         //open that window
         onResume();
     }
 
     public void resetGame(View view){
+        Score.Score = 0;
         Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage( getBaseContext().getPackageName() );
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
+    }
+
+    public void pauseGame(View view){
+        if(Paused) {
+            Paused = false;
+            onResume();
+        }
+        else {
+            Paused = true;
+            onPause();
+        }
     }
 }
